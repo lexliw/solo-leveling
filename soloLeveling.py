67814cfe1d35c71adb2667c51f6bc7929fce7cf5
Alex Liauw <lexliw@gmail.com>
2025-04-19 15:58:09 -0300

    ajuste home

--- soloLeveling.py
+++ soloLeveling.py
@@ -382,7 +382,7 @@ conteudo = ''
 for folder in onlyfolders:
     conteudo += f'<a href="/solo-leveling/{folder}/index.html">{folder}</a>'
 
-with open('template-menu.html', 'r') as f:
+with open('../liwrary/template-menu.html', 'r') as f:
     template = f.readlines()
 
 for i in range(len(template)):
@@ -416,7 +416,7 @@ for i in range(len(onlyfolders)):
         conteudo += f'<img src="{file}"/>'
 
     
-    with open('template-capitulo.html', 'r') as f:
+    with open('../liwrary/template-capitulo.html', 'r') as f:
         template = f.readlines()
 
     for x in range(len(template)):
